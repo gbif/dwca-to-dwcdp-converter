57ec028b802cc1f3b97927f3de5d6df9fa91a225
Mikhail Podolskiy <mike.podolskiy90@gmail.com>
2025-03-31 11:16:30 +0200

    Fix paths at start.sh script

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 mvn clean package
 
-java -jar dwc-to-dwcdp-converter-cli/target/dwca-to-dwcdp-converter-cli.jar dwca-to-dwcdp-converter --log-config dwc-to-dwcdp-converter-cli/src/main/resources/logback.xml
+java -jar dwca-to-dwcdp-converter-cli/target/dwca-to-dwcdp-converter-cli.jar dwca-to-dwcdp-converter --log-config dwca-to-dwcdp-converter-cli/src/main/resources/logback.xml
