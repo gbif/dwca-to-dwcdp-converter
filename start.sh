d06f6fa4592d8693502686deab006a20d4829ce1
Mikhail Podolskiy <mike.podolskiy90@gmail.com>
2025-04-14 14:14:19 +0200

    Revert start.sh script

--- start.sh
+++ start.sh
@@ -1,8 +1,6 @@
-#mvn clean package
-
 java -agentlib:jdwp="transport=dt_socket,server=y,suspend=n,address=*:5005" \
       -jar dwca-to-dwcdp-converter-cli/target/dwca-to-dwcdp-converter-cli.jar dwca-to-dwcdp-converter \
       --log-config dwca-to-dwcdp-converter-cli/src/main/resources/logback.xml \
-      --dwca /Users/rvl320/IdeaProjects/dwca-to-dwcdp-converter/input-dwca/dwca-aves-tanzanian-v1.9.zip \
-      --dwcdp /Users/rvl320/IdeaProjects/dwca-to-dwcdp-converter/output-dwcdp \
-      --mappings /Users/rvl320/IdeaProjects/dwca-to-dwcdp-converter/dwca-to-dwcdp-converter-common/src/main/resources/dwca-to-dwcdp-mappings
+      --dwca ... \
+      --dwcdp ... \
+      --mappings dwca-to-dwcdp-converter-common/src/main/resources/dwca-to-dwcdp-mappings
