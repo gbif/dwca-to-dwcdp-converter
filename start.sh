7b9935d4ed1535d7b5cd767196adf0d46ed4411c
Mikhail Podolskiy <mike.podolskiy90@gmail.com>
2025-04-14 14:06:12 +0200

    Update start.sh script

--- start.sh
+++ start.sh
@@ -1,3 +1,6 @@
-mvn clean package
-
-java -jar dwca-to-dwcdp-converter-cli/target/dwca-to-dwcdp-converter-cli.jar dwca-to-dwcdp-converter --log-config dwca-to-dwcdp-converter-cli/src/main/resources/logback.xml
+java -agentlib:jdwp="transport=dt_socket,server=y,suspend=n,address=*:5005" \
+      -jar dwca-to-dwcdp-converter-cli/target/dwca-to-dwcdp-converter-cli.jar dwca-to-dwcdp-converter \
+      --log-config dwca-to-dwcdp-converter-cli/src/main/resources/logback.xml \
+      --dwca ... \
+      --dwcdp ... \
+      --mappings dwca-to-dwcdp-converter-common/src/main/resources/dwca-to-dwcdp-mappings
